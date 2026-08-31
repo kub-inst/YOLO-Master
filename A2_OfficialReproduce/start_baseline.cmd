aa768cab6a152eebf24216127a8ffc2ec7eb9238
kub-inst <1392585871@qq.com>
2026-08-31 23:45:18 +0800

    experiments under official protocol

---
+++ A2_OfficialReproduce/start_baseline.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd /d "D:\coding\YOLO-Master"
+"C:\Users\yqy\.conda\envs\yolo_master\Scripts\yolo.exe" train cfg="A2_OfficialReproduce\default.yaml" project="A2_OfficialReproduce\runs" name="baseline"
+
